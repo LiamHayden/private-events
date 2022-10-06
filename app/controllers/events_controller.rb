e49cfbd3bbe2476e0dbaf8d48e864a88284b27ea
Liam Hayden <l.hayden67@outlook.com>
2022-10-06 13:55:54 +0100

    Add show view and action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,10 @@ class EventsController < ApplicationController
 
 	end
 
+	def show
+		@event = Event.find(params[:id])
+	end
+
 	private
 	def event_params
 		params.require(:event).permit(:name, :location, :date)
