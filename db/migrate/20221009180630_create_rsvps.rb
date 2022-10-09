13148a1addc97ff4c4fe09e1500461fb142cf3bb
Liam Hayden <65085447+LiamHayden@users.noreply.github.com>
2022-10-09 19:17:42 +0100

    Add/rsvp through table (#13)
    * Remove unwanted migration file
    
    * Add rsvp through table and references

--- db/migrate/20221009180630_create_rsvps.rb
+++ db/migrate/20221009180630_create_rsvps.rb
@@ -1,8 +1,10 @@
-class AttendedEvents < ActiveRecord::Migration[7.0]
+class CreateRsvps < ActiveRecord::Migration[7.0]
   def change
-    create_table :attended_events do |t|
+    create_table :rsvps do |t|
       t.references :attendee, null: false, foreign_key: { to_table: :users }, index: true
       t.references :attended_event, null: false, foreign_key: { to_table: :events }, index: true
+
+      t.timestamps
     end
   end
 end
