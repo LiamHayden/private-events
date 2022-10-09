7eae12b6642866333feddce5f6a6842cca20ea58
Liam Hayden <l.hayden67@outlook.com>
2022-10-09 19:30:34 +0100

    Update user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :creator_events, foreign_key: "creator_id", class_name: "Event"
-  has_many :attended_events, foreign_key: "attendee_id"
+  has_many :rsvps, foreign_key: "event_attendee_id"
+  has_many :attended_events, through: :rsvps
 end
