dba13c89a737bf636cf42719f062fdd8e3635980
Liam Hayden <l.hayden67@outlook.com>
2022-10-10 13:22:39 +0100

    Add rsvp action and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'events#index'
   
-  resources :users, :events
+  resources :users
+  resources :events, shallow: true do
+    resources :rsvps
+  end
 end
