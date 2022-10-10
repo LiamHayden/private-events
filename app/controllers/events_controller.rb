679d85a250f420e1df2017a0c8b0b6dd6c71790c
Liam Hayden <65085447+LiamHayden@users.noreply.github.com>
2022-10-10 16:27:46 +0100

    Update/rsvp associations (#14)
    * Add rsvp associations
    
    * Update user associations
    
    * Update event associations
    
    * Add rsvp action and route
    
    * Fix routes, associations, controller, and add controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,6 +18,16 @@ class EventsController < ApplicationController
 		@event = Event.find(params[:id])
 	end
 
+	def rsvp
+		@event = Event.find(params[:id])
+		if @event.attendees.include?(current_user)
+			redirect_to @event, notice: "You are already attending the event."
+		else
+			@event.attendees << current_user
+			redirect_to @event, notice: "You are now attending the event."
+		end
+	end
+
 	private
 	def event_params
 		params.require(:event).permit(:name, :location, :date)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'events#index'
   
-  resources :users, :events
+  resources :users
+  resources :events do
+    member do
+      get 'rsvp'
+    end
+  end
 end
