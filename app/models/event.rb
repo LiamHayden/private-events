a632b46fa6f6b6c01a07b83d0ebbd1df36329093
Liam Hayden <l.hayden67@outlook.com>
2022-10-15 16:34:25 +0100

    Add past and upcoming class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :rsvps, foreign_key: "attended_event_id"
   has_many :attendees, through: :rsvps
+
+  def self.past
+    self.where("date < ?", Date.today)
+  end
+
+  def self.upcoming
+    self.where("date >= ?", Date.today)
+  end
 end
